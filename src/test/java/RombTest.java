0e810941eefe1cd973268ac5ae76e4e56db1c119
Bodnár <bodnarbence2002@gmail.com>
2024-01-10 10:56:37 +0100

    test 5

--- src/test/java/RombTest.java
+++ src/test/java/RombTest.java
@@ -1,4 +1,7 @@
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import lan.zold.Romb;
 
 public class RombTest {
     Romb romb;
@@ -10,7 +13,7 @@ public class RombTest {
 
     @Test
     public void testCalcArea() {
-        this.romb.calcArea(30.0);
+        this.romb.calcArea(30.0, 90);
     }
 
 }
