da29b5bf7fd67106520360c453e6b5ebc4fefd77
Bodnár <bodnarbence2002@gmail.com>
2024-01-10 11:17:47 +0100

    test 9

--- src/main/java/lan/zold/Romb.java
+++ src/main/java/lan/zold/Romb.java
@@ -2,7 +2,7 @@ package lan.zold;
 
 public class Romb {
     public double calcArea(double side, double degree) {
-        return Math.pow(side, 2) * Math.sin(side);
+        return Math.pow(side, 2) * Math.sin(degree);
     }
 
     public double calcCircumference(double side) {
--- src/test/java/RombTest.java
+++ src/test/java/RombTest.java
@@ -15,14 +15,14 @@ public class RombTest {
     @Test
     public void testCalcArea() {
         double actual = this.romb.calcArea(30.0, 70);
-        double expected = 0.0;
+        double expected = 696.501613402;
         Assert.assertEquals(actual, expected, 0.1);
     }
 
     @Test
     public void testCalcCircumference() {
         double actual = this.romb.calcCircumference(30.0);
-        double expected = 0.0;
+        double expected = 120;
         Assert.assertEquals(actual, expected, 0.1);
     }
 
